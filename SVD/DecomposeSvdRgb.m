cbdf97f67a0c07f3786feed4490eac6d997bb604
ankushkawanpure <ankushkawanpure@gmail.com>
2016-02-14 23:44:31 -0500

    project files

---
+++ SVD/DecomposeSvdRgb.m
@@ -0,0 +1,59 @@
+function [U,S,V]= DecomposeSvdRgb(TextName, Coordinates, Zoom)
+
+% This function is used to decomposes the input data according to SVD algorithm
+% (RGB)   
+%TextName:  name of the dynamic texture;                             
+%Coordinates-  Width by Height
+%Zoom- Zoom factor used to Zoom in images; 
+%This Function returns Decomposition matrices-[U,S,V]     
+                                          
+name = [TextName,'.avi']; %to assign name to avi file
+p =aviinfo(name)          % to get information of avi file 
+
+FirstRow = Coordinates(1) %First Height coordinate
+LastRow = Coordinates(2)  %last Height coordinate 
+FirstColumn = Coordinates(3) %First Width coordinate
+LastColumn = Coordinates(4)  %last Width coordinate
+
+mov = aviread(name); % mov = aviread(filename) reads the AVI movie filename into the MATLAB movie structure mov. 
+tau = length(mov);   % Store no of frames in tau variable.
+im = getfield(mov,{1,1},'cdata'); %Get field of structure array
+[x y z] = size(imresize(im(FirstRow:LastRow,FirstColumn:LastColumn,:),1/Zoom));% Resize Image into 1/ZOOM times original Image and stored size in x y z
+
+tic            %tic starts a stopwatch timer.
+for i = 1:tau   %For loop is used for all frames
+    im = getfield(mov,{1,i},'cdata');
+    TempMat = double(imresize(im(FirstRow:LastRow,FirstColumn:LastColumn,:),1/Zoom));  % returns an image that is 1/Zoom times the size of Image.& Convert to double precision
+    imshow(uint8(TempMat)); %Unit8 convert to unsigned  8 bit integer
+    getframe;           %getframe returns a movie frame. The frame is a snapshot (pixmap) of the current axes or figure. 
+    Srgb(:,i) = TempMat(:); %Convert in to column vector
+end
+
+Amean = mean(Srgb,2); %d-Vector to store  the average mean values of the elements along different dimensions of an array. 
+
+[U,S,V] = svd(Srgb-Amean*ones(1,size(Srgb,2)),0); %The svd command computes the matrix singular value decomposition.
+
+%%
+%This is my addition trying to speed up the video a bit by factor seek
+% seek=1.5;
+% 
+% S=S*seek;
+% V=V*seek;
+
+
+
+%%
+
+
+toc     %prints the elapsed time since tic was used. 
+save([  'D:/Database/D_SVD_RGB_',TextName,'_',num2str(FirstRow),'x',num2str(LastRow),'x',...
+        num2str(FirstColumn),'x',num2str(LastColumn),'_Zoom',num2str(Zoom),'.mat'],...
+        'U', 'S', 'V', 'Srgb', 'Amean','x','y','z','tau');
+         
+        %Save Parameters in Daabase
+        
+        
+        
+        
+        
+        
---
+++ SVD/SvdRgb.m
@@ -0,0 +1,54 @@
+function [PsnrSvdRgb, CostSvdRgb] = SvdRgb(TextName,Coordinates, Zoom, Nrgb)
+
+%This Function is used to Synthesize & calculate the no of model coefficients ie cost
+%factor and PSNR for SVD Algorithm
+%TextName:name of the dynamic texture;               
+%Coordinates:coordinates of the portion of the image 
+%Zoom:Zoom factor to apply to the images; 
+%Nrgb:temporal order of the model ie Nrgb= 5:5:tau-5
+%This  Function returns Average prediction error and no of model
+%coefficients ie PsnrSvdRgb & CostSvdRgb: number of model coefficients
+ 
+FirstRow =Coordinates(1);
+LastRow =Coordinates(2);
+FirstColumn =Coordinates(3);
+LastColumn =Coordinates(4);
+
+load([  'D:/Database/D_SVD_RGB_',TextName,'_',num2str(FirstRow),'x',num2str(LastRow),'x',...
+        num2str(FirstColumn),'x',num2str(LastColumn),'_Zoom',num2str(Zoom),'.mat']);
+      %load decompose parameters from Database
+Nvrgb = round(Nrgb/3*2);   % Nrgb 5 to tau-5
+tic
+
+    for j = 1:length(Nrgb) %j 1 to lenght(Nrgb)
+    
+    n_rgb = Nrgb(j);    %copy 5:10:Tau-5 to n_rgb
+    nv_rgb = Nvrgb(j);   %copy 3,7,10...  to nv_rgb  
+    first = 1:n_rgb;          % 1 to -- n_rgb
+    
+    Chat_rgb = U(:,first);   %Collecting 1 to first  column of U 
+    Xhat_rgb = S(first,first)*V(:,first)'; %collecting first n column of V and first n diagonal elments of of S get Product term
+    X1 = Xhat_rgb(:,1:tau-1);
+    X2 = Xhat_rgb(:,2:tau);
+    Ahat_rgb = X2*pinv(X1);   %Moore-Penrose pseudoinverse of a matrix
+    
+    for k = 2:tau             %1 to tau-1
+    X_rgb = Ahat_rgb*Xhat_rgb(:,k-1);
+    Y_rgb_synth = Chat_rgb*X_rgb+Amean;  %+Amean This is the formula for synthesis ie yk=Cxk + d
+      
+    MSE_rgb = mse( Srgb(:,k) - Y_rgb_synth ); %to find MSE(Original rgb frame - predicted rgb frame) 
+    PsnrSvdRgb(j,k-1) = 10*log10( (255^2) / MSE_rgb); % Average value of the PEAK Signal to Noise Ratio(PSNR)-Average Prediction error.
+         %17 by 88 for flame
+    imshow(uint8(reshape(Y_rgb_synth,x,y,z)));
+        getframe;
+        
+    end  
+    
+    CostSvdRgb(j) = prod(size(Ahat_rgb)) + prod(size(Chat_rgb)) + prod(size(Amean))+nv_rgb*n_rgb ;% number of model coefficients for 1 to tau-5 (ie 5 10 15 ...)
+    
+end
+toc
+save([  'D:/Database/CP_SVD_RGB_',TextName,'_',num2str(FirstRow),'x',num2str(LastRow),'x',...
+        num2str(FirstColumn),'x',num2str(LastColumn),'_Zoom',num2str(Zoom),'.mat'],...
+        'PsnrSvdRgb', 'CostSvdRgb',  'Nrgb');
+    %store PSNR,COST,Nrgb values in Database.
